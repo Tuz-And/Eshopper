fd7070ada510a93f14422f007b1fe881fecc9eae
Tuz <kozurnuj89@gmail.com>
2020-11-24 00:57:45 +0200

    pages done

--- pages/urls.py
+++ pages/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('404', views.error, name='error'),
+    path('contact-us', views.contact_us, name='contact_us'),
 
     
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,3 +2,13 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'pages/index.html')
+
+
+def error(reqest):
+    return render(reqest,"pages/404.html")
+
+
+def contact_us(reqest):
+    return render(reqest,"pages/contact-us.html")
+
+
