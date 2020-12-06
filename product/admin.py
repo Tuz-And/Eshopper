87a237b9c0bf9440488f5c63435cfbacff4a4992
Tuz <kozurnuj89@gmail.com>
2020-12-06 22:35:03 +0200

    'pagination'

---
+++ product/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Product
+# Register your models here.
+
+
+
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', "tittle", "is_published", "price", "list_date")
+    list_diaplay_links = ('id', "tittle")
+    list_filter = ("category",)
+    list_editable = ("is_published",)
+    search_fields = ("tittle", "price", "description", "price", "category")
+    list_per_page = 25
+
+
+admin.site.register(Product, ProductAdmin)
