d76e739b9e41a6cd8a92b5767edb38f0a3730395
Tuz <kozurnuj89@gmail.com>
2020-12-13 16:41:33 +0200

    logaut register dashboard

--- pages/urls.py
+++ pages/urls.py
@@ -5,6 +5,10 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('404', views.error, name='error'),
     path('contact-us', views.contact_us, name='contact_us'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+    path('register', views.register, name='register'),
+    path('dashboard', views.dashboard, name='dashboard'),
 
     
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
 from product.models import Product
+from django.contrib.auth.models import User
+from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     product = Product.objects.order_by('-list_date').filter(is_published=True)
@@ -21,4 +24,62 @@ def error(reqest):
 def contact_us(reqest):
     return render(reqest,"pages/contact-us.html")
 
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'User logged in')
+            messages.error(request, 'error test')
+            messages.warning(request, 'warning test')
 
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Incorrect login or password')
+            return redirect('login')
+
+    return render(request, 'pages/login.html')
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, "Logged out")
+    return redirect('index')
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        passwordConfirm = request.POST['passwordConfirm']
+
+        user_condition = not User.objects.filter(username=username).exists()
+        password_condition = password == passwordConfirm
+        email_condition = not User.objects.filter(email=email).exists()
+
+        if user_condition and password_condition and email_condition:
+            user = User.objects.create_user(
+                username=username,
+                password=password,
+                email=email,
+            )
+            user.save()
+            messages.success(request, 'User registered')
+            return redirect('login')
+        else:
+            if not user_condition:
+                messages.error(request, "User with login '" +
+                               username + "' exists")
+            if not password_condition:
+                messages.error(request, "Passwords do not match")
+            if not email_condition:
+                messages.error(request, "User with email '"+email+"' exists")
+            return redirect("register")
+
+    return render(request, 'pages/register.html')
+
+def dashboard(request):
+    return render(request, "pages/dashboard.html")
